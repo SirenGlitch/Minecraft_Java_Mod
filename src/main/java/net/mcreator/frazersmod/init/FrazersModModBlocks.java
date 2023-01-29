3cd8d7c6e4a825235741c2e9deff11d22adeabeb
SirenGlitch <frazerjamesrose@outlook.com>
2023-01-29 08:47:25 +0000

    Added Hot Wheels

--- src/main/java/net/mcreator/frazersmod/init/FrazersModModBlocks.java
+++ src/main/java/net/mcreator/frazersmod/init/FrazersModModBlocks.java
@@ -7,13 +7,27 @@ package net.mcreator.frazersmod.init;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.level.block.Block;
 
 import net.mcreator.frazersmod.block.TestBlockBlock;
+import net.mcreator.frazersmod.block.HotWheelsBlock;
 import net.mcreator.frazersmod.FrazersModMod;
 
 public class FrazersModModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, FrazersModMod.MODID);
 	public static final RegistryObject<Block> TEST_BLOCK = REGISTRY.register("test_block", () -> new TestBlockBlock());
+	public static final RegistryObject<Block> HOT_WHEELS = REGISTRY.register("hot_wheels", () -> new HotWheelsBlock());
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientSideHandler {
+		@SubscribeEvent
+		public static void clientSetup(FMLClientSetupEvent event) {
+			HotWheelsBlock.registerRenderLayer();
+		}
+	}
 }
--- src/main/java/net/mcreator/frazersmod/init/FrazersModModItems.java
+++ src/main/java/net/mcreator/frazersmod/init/FrazersModModItems.java
@@ -22,6 +22,7 @@ public class FrazersModModItems {
 	public static final RegistryObject<Item> TEST_BLOCK = block(FrazersModModBlocks.TEST_BLOCK, CreativeModeTab.TAB_MISC);
 	public static final RegistryObject<Item> COLA = REGISTRY.register("cola", () -> new ColaItem());
 	public static final RegistryObject<Item> CALL_ME_DRILL = REGISTRY.register("call_me_drill", () -> new CallMeDrillItem());
+	public static final RegistryObject<Item> HOT_WHEELS = block(FrazersModModBlocks.HOT_WHEELS, CreativeModeTab.TAB_BUILDING_BLOCKS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
